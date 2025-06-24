7b6b909ff5b97cf360b6a72575ab3ae8b7d07ec8
Tongducduy03 <tongducduy4@gmail.com>
2025-06-24 16:03:40 +0700

    401

--- backend/src/models/Idea.ts
+++ backend/src/models/Idea.ts
@@ -4,7 +4,7 @@ export interface IIdea extends Document {
   fullName?: string;
   department: string;
   idea: string;
-  solution: string;
+  // solution: string;
   ideaCode: string;
   submissionDate: Date;
   isPaid: boolean;
@@ -14,7 +14,7 @@ const IdeaSchema: Schema = new Schema({
   fullName: { type: String, required: false },
   department: { type: String, required: true },
   idea: { type: String, required: false },
-  solution: { type: String, required: false },
+  // solution: { type: String, required: false },
   ideaCode: { type: String, required: true, unique: true },
   submissionDate: { type: Date, default: Date.now },
   isPaid: { type: Boolean, default: false }
